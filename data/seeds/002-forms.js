ab508a1ca3763a168bb265b83a00da82a57d2c9a
mattr21 <matt.rothstein21@gmail.com>
2019-04-27 18:37:12 -0700

    added form_fields seed data

--- data/seeds/002-forms.js
+++ data/seeds/002-forms.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex) {
     return knex('forms').insert([
-      {user_id: 1, name: 'FE dev',}, // 1
-      {user_id: 2, name: 'BE dev',}, // 2
+        {user_id: 1, name: 'FE dev',}, // 1
+        {user_id: 2, name: 'BE dev',}, // 2
     ]);
   };
