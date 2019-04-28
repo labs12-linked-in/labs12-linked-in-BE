9df0985217cd1cb40f7e33fb42bba4cf66e3a102
mattr21 <matt.rothstein21@gmail.com>
2019-04-27 18:35:05 -0700

    added users seed data

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,1 +1,18 @@
+const bcrypt = require('bcryptjs')
 
+exports.seed = function(knex) {
+  return knex('users').insert([
+    {
+      first_name: 'Matt',
+      last_name: 'Rothstein',
+      email: 'matt@test.com',
+      password: bcrypt.hashSync('matt', 4),
+    }, // 1
+    {
+      first_name: 'Brandon',
+      last_name: 'Borrero',
+      email: 'brandon@test.com',
+      password: bcrypt.hashSync('brandon', 4),
+    }, // 2
+  ]);
+};
