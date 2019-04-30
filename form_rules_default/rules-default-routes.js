6f017ce6fc94d725c5d2f1f7f465be20152241cc
mattr21 <matt.rothstein21@gmail.com>
2019-04-30 14:11:41 -0700

    made admin_email for departments notnullable

---
+++ form_rules_default/rules-default-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const RulesDefault = require('./rules-default-models.js');
+
+// add a new form_rules_default
+router.post('/:userId/:formId', async (req, res) => {
+    
+}) 
