a3a110c8d84cc56c48af55e5624f9d917eda7531
Brandon L. Borrero <borrero509@outlook.com>
2019-04-30 08:11:15 -0700

    removed email key

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -3,16 +3,14 @@ const bcrypt = require('bcryptjs')
 exports.seed = function(knex) {
   return knex('users').insert([
     {
-        first_name: 'Matt',
-        last_name: 'Rothstein',
-        email: 'matt@test.com',
-        password: bcrypt.hashSync('matt', 4),
+      first_name: 'Matt',
+      last_name: 'Rothstein',
+      password: bcrypt.hashSync('matt', 4)
     }, // 1
     {
-        first_name: 'Brandon',
-        last_name: 'Borrero',
-        email: 'brandon@test.com',
-        password: bcrypt.hashSync('brandon', 4),
-    }, // 2
-  ]);
-};
+      first_name: 'Brandon',
+      last_name: 'Borrero',
+      password: bcrypt.hashSync('brandon', 4)
+    } // 2
+  ])
+}
