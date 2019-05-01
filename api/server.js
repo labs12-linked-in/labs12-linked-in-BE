35b382bebbc01798d6ca9b040b6137061e3f9de9
cori-peck <cori.peck@ymail.com>
2019-05-01 11:42:18 -0500

    implemented passport into auth and added cookies

--- api/server.js
+++ api/server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const cookiesMiddleware = require('universal-cookie-express');
 
 const configureMiddleware = require('./middleware.js');
 const authRouter = require('../authenticate/auth-routes.js');
+const forms = require('../forms/forms-routes.js')
 
 const server = express();
+require('../config/passport');
+
+configureMiddleware(server);
 
 //express middleware to parse req.body before handlers
 server.use(bodyParser.json());
 
-const forms = require('../forms/forms-routes.js')
 
-configureMiddleware(server);
+server.use(cookiesMiddleware());
+server.use(session({ secret: 'turquoise monkey', resave: false, saveUninitialized: true, cookie: { secure: true} }));
+// Initializing passport and passport.session() middleware to support persistent login sessions
+//server.use(passport.session());
+server.use(passport.initialize());
+
 
 server.get('/', (req, res) => {
   res.status(200).send('Hello Earthling')
