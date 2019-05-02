fd431d5a5f8a376aef03c60b0ac0d0f559ef678b
cori-peck <cori.peck@ymail.com>
2019-05-02 15:38:02 -0500

    solving passport issues

--- authenticate/auth-routes.js
+++ authenticate/auth-routes.js
@@ -28,6 +28,13 @@ router.get('/logout', function(req, res){
     res.redirect('/login');
 })
 
+router.get('/authenticate',
+    function(req, res) {
+        const user = req.user
+        res.status(200).json(user)
+    }
+)
+
 
 //route middleware that checks if user is authenticated
 //use on any resource that is restricted.
