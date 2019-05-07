23e8cc096d16de8b73b73c94d610910894be05e8
Brandon L. Borrero <borrero509@outlook.com>
2019-05-07 10:23:06 -0700

    added triggers to form_fields table

--- data/migrations/20190424215805_initial_tables.js
+++ data/migrations/20190424215805_initial_tables.js
@@ -1,145 +1,153 @@
 exports.up = function(knex) {
   return knex.schema
-    .createTable('users', table => {
-      table.increments()
-      table.string('first_name').notNullable()
-      table.string('last_name').notNullable()
+    .createTable("users", table => {
+      table.increments();
+      table.string("first_name").notNullable();
+      table.string("last_name").notNullable();
       table
-        .string('user_id')
+        .string("user_id")
         .notNullable()
-        .unique()
+        .unique();
       table
-        .datetime('created_at')
+        .datetime("created_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
       table
-        .datetime('updated_at')
+        .datetime("updated_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
     })
 
-    .createTable('forms', table => {
-      table.increments()
+    .createTable("forms", table => {
+      table.increments();
       table
-        .integer('user_id')
+        .integer("user_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-      table.string('name')
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("name");
+      table.integer("field_count");
       table
-        .datetime('created_at')
+        .datetime("created_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
       table
-        .datetime('updated_at')
+        .datetime("updated_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
     })
 
-    .createTable('form_fields', table => {
-      table.increments()
+    .createTable("form_fields", table => {
+      table.increments();
       table
-        .integer('form_id')
+        .integer("form_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('forms')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-      table.string('name').notNullable()
-      table.string('type').notNullable()
-      table.string('selected').notNullable()
+        .references("id")
+        .inTable("forms")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("name").notNullable();
+      table.string("type").notNullable();
+      table.string("selected").notNullable();
       table
-        .datetime('created_at')
+        .datetime("created_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
       table
-        .datetime('updated_at')
+        .datetime("updated_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
+    })
+    .raw(
+      " CREATE OR REPLACE FUNCTION add_one() RETURNS trigger LANGUAGE plpgsql AS $$ BEGIN UPDATE forms SET field_count = field_count + 1 WHERE id = NEW.form_id; RETURN NEW; END; $$; CREATE TRIGGER add AFTER INSERT ON form_fields FOR EACH ROW EXECUTE FUNCTION add_one();"
+    )
+    .raw(
+      " CREATE OR REPLACE FUNCTION subtract_one() RETURNS trigger LANGUAGE plpgsql AS $$ BEGIN UPDATE forms SET field_count = field_count - 1 WHERE id = OLD.form_id; RETURN NEW; END;  $$; CREATE TRIGGER subtract BEFORE DELETE ON form_fields FOR EACH ROW EXECUTE FUNCTION subtract_one();"
+    )
+
+    .createTable("form_rules_default", table => {
+      table.increments();
+      table
+        .integer("form_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("forms")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("send_to").notNullable();
+      table
+        .datetime("created_at")
+        .notNullable()
+        .defaultTo(knex.fn.now());
+      table
+        .datetime("updated_at")
+        .notNullable()
+        .defaultTo(knex.fn.now());
     })
 
-    .createTable('form_rules_default', table => {
-      table.increments()
+    .createTable("form_rules", table => {
+      table.increments();
       table
-        .integer('form_id')
+        .integer("form_field_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('forms')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-      table.string('send_to').notNullable()
-      table
-        .datetime('created_at')
-        .notNullable()
-        .defaultTo(knex.fn.now())
-      table
-        .datetime('updated_at')
-        .notNullable()
-        .defaultTo(knex.fn.now())
-    })
-
-    .createTable('form_rules', table => {
-      table.increments()
-      table
-        .integer('form_field_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('form_fields')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
+        .references("id")
+        .inTable("form_fields")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
       // table.string('name').notNullable()
-      table.string('operator').notNullable()
-      table.string('text_compare')
-      table.integer('number_compare')
-      table.string('send_to').notNullable()
+      table.string("operator").notNullable();
+      table.string("text_compare");
+      table.integer("number_compare");
+      table.string("send_to").notNullable();
       table
-        .datetime('created_at')
+        .datetime("created_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
       table
-        .datetime('updated_at')
+        .datetime("updated_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
     })
 
-    .createTable('departments', table => {
-      table.increments()
+    .createTable("departments", table => {
+      table.increments();
       table
-        .integer('user_id')
+        .integer("user_id")
         .unsigned()
         .notNullable()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-      table.string('name').notNullable()
-      table.string('admin_email').notNullable()
-      table.string('supervisor_email')
-      table.string('manager_email')
-      table.string('director_email')
-      table.string('vp_email')
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("name").notNullable();
+      table.string("admin_email").notNullable();
+      table.string("supervisor_email");
+      table.string("manager_email");
+      table.string("director_email");
+      table.string("vp_email");
       table
-        .datetime('created_at')
+        .datetime("created_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
+        .defaultTo(knex.fn.now());
       table
-        .datetime('updated_at')
+        .datetime("updated_at")
         .notNullable()
-        .defaultTo(knex.fn.now())
-    })
-}
+        .defaultTo(knex.fn.now());
+    });
+};
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('users')
-    .dropTableIfExists('departments')
-    .dropTableIfExists('forms')
-    .dropTableIfExists('form_fields')
-    .dropTableIfExists('form_rules_default')
-}
+    .dropTableIfExists("form_rules")
+    .dropTableIfExists("departments")
+    .dropTableIfExists("form_rules_default")
+    .dropTableIfExists("form_fields")
+    .dropTableIfExists("forms")
+    .dropTableIfExists("users");
+};
--- user/user-routes.js
+++ user/user-routes.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const User = require('./user-model');
-const bcrypt = require('bcryptjs');
-const axios = require('axios');
+const router = require("express").Router();
+const User = require("./user-model");
+const bcrypt = require("bcryptjs");
+const axios = require("axios");
 
 // verify if user exists, if so return user if not add user then return user
-router.get('/user', async (req,res) => {
+router.get("/user", async (req, res) => {
   try {
     const users = await Users.find(req.query);
     res.status(200).json(users);
@@ -13,80 +13,62 @@ router.get('/user', async (req,res) => {
   }
 });
 
-router.post('/user', async (req, res) => {
-  // console.log("\n ********* ACCESS_TOKEN ********* \n", req.body.result.access_token)
-  const response = await axios
-    .get('https://api.linkedin.com/v2/me', {
-      headers: {
-        Authorization: 'Bearer ' + req.body.result.access_token,
-        Connection: 'Keep-Alive'
-      }
-    })
-    // console.log(response);
-    // console.log(response.data.id);
-    // res.status(200).json(response);
+router.post("/user", async (req, res) => {
+  const response = await axios.get("https://api.linkedin.com/v2/me", {
+    headers: {
+      Authorization: "Bearer " + req.body.result.access_token,
+      Connection: "Keep-Alive"
+    }
+  });
 
-  // console.log(response.data.localizedLastName)
-  const { id } = response.data
-  const firstName = response.data.localizedFirstName
-  const lastName = response.data.localizedLastName
+  const { id } = response.data;
+  const firstName = response.data.localizedFirstName;
+  const lastName = response.data.localizedLastName;
 
-  console.log(id)
-  console.log(firstName)
-  console.log(lastName)
-
+  console.log(id);
+  console.log(firstName);
+  console.log(lastName);
 
   if (!id) {
-    res.status(400).json({ message: 'Please provide user id.' })
+    res.status(400).json({ message: "Please provide user id." });
   }
 
-  User.verifyUser('wpmRPZ3gcX')
+  User.verifyUser(id)
     .then(async user => {
-      // console.log("THIS ONE", user)
       if (user == null) {
-        const newUser = await User.addUser({ user_id: 'wpmRPZ3gcX', first_name: 'sdsk', last_name: 'ldnk'})
-        res.status(200).json(newUser)
+        const newUser = await User.addUser({
+          user_id: id,
+          first_name: firstName,
+          last_name: lastName
+        });
+        res.status(200).json(newUser);
       }
-      res.status(200).json(user)
-      // console.log("KSJBSJKB", user)
+      res.status(200).json(user);
     })
     .catch(error => {
-      res.status(500).json({ err: 'Could not verify user', error })
-    })
-})
+      res.status(500).json({ err: "Could not verify user", error });
+    });
+});
 
 // delete user
-router.delete('/banish', (req, res) => {
-  const { user_id } = req.body
+router.delete("/banish", (req, res) => {
+  const { user_id } = req.body;
 
   if (!user_id) {
-    res.status(400).json({ message: 'Please provide user id.' })
+    res.status(400).json({ message: "Please provide user id." });
   }
 
   User.deleteUser(user_id)
     .then(response => {
-      console.log(res, 'res')
+      console.log(res, "res");
       res
         .status(200)
-        .json({ message: 'user has been banished from this database' })
+        .json({ message: "user has been banished from this database" });
     })
     .catch(error => {
-      console.log(error, 'er')
-      res.status(500).json({ err: 'Could not delete user', error })
-    })
-})
+      console.log(error, "er");
+      res.status(500).json({ err: "Could not delete user", error });
+    });
+});
 
-// get user data from linkedin
-// router.post('/data', async (req, res) => {
-//   console.log("\n ********* ACCESS_TOKEN ********* \n", req.body.result.access_token)
-//   const response = await axios
-//     .get('https://api.linkedin.com/v2/me', {
-//       headers: {
-//         Authorization: 'Bearer ' + req.body.result.access_token
-//       }
-//     })
-//     console.log(response);
-//     res.status(200).json(response);
-// });
-
-module.exports = router
+module.exports = router;
