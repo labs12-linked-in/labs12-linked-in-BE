2d58df4127076894768199dc6f51d4f23be76385
Yanrong Xu <xuyanrong@icloud.com>
2019-05-14 13:35:47 -0400

    Revert "changed seeds"

--- data/seeds/003-form_fields.js
+++ data/seeds/003-form_fields.js
@@ -1,24 +1,34 @@
 exports.seed = function(knex) {
-  return knex('form_fields').insert([
-    {
-      form_id: 1,
-      name: 'Picture'
-    },
-    {
-      form_id: 1,
-      name: 'Name'
-    },
-    {
-      form_id: 2,
-      name: 'Headline'
-    },
-    {
-      form_id: 2,
-      name: 'Location'
-    },
-    {
-      form_id: 2,
-      name: 'Total Endorsements'
-    }
-  ])
-}
+    return knex('form_fields').insert([
+        {
+            form_id: 1,
+            name: 'Picture',
+            type: 'image',
+            selected: '.pv-top-card-section__photo'
+        },
+        {
+            form_id: 1,
+            name: 'Name',
+            type: 'text',
+            selected: '.pv-top-card-section__name',
+        },
+        {
+            form_id: 2,
+            name: 'Headline',
+            type: 'text',
+            selected: '.pv-top-card-section__headline',
+        },
+        {
+            form_id: 2,
+            name: 'Location',
+            type: 'text',
+            selected: '.pv-top-card-section__location',
+        },
+        {
+            form_id: 2,
+            name: 'Total Endorsements',
+            type: 'number',
+            selected: '.pv-skill-category-entity__endorsement-count',
+        },
+    ]);
+    };
